ef40e34053ec3b23f883b60355f4c287121d0c41
Alex <mrsweety380@gmail.com>
2023-09-14 19:27:58 +0300

    Misc 1. Fixed module's name 2. Global font Gotham, _tygography.less & _theme.less 3. Added folder font

--- app/code/Alex/Lesson4FrontendHomework/registration.php
+++ app/code/Alex/Lesson4FrontendHomework/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Lesson4FrontendHomework',
+    'Alex_Lesson4FrontendHomework',
     __DIR__
 );
