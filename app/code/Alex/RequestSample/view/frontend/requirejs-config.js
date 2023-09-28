4bf021f08a8814e03ccadd36a5ea7db1541f9985
Alex Salnikov <alexSal@gmail.com>
2023-09-28 12:04:16 +0300

    Replaced the jquery.cookie component with the newer implementation

--- app/code/Alex/RequestSample/view/frontend/requirejs-config.js
+++ app/code/Alex/RequestSample/view/frontend/requirejs-config.js
@@ -2,7 +2,9 @@ var config = {
     map: {
         '*': {
             alex_requestSample: 'Alex_RequestSample/js/request-sample',
-            alex_validationAlert: 'Alex_RequestSample/js/validation-alert'
+            alex_validationAlert: 'Alex_RequestSample/js/validation-alert',
+            // overriding default cookie component
+            'jquery/jquery.cookie': 'Alex_RequestSample/js/jquery/jquery.cookie'
         }
     }
 };
