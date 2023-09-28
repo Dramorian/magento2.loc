1fa5f91d973b0a0a5ec6e15b953039f3a8e0d473
Alex Salnikov <alexSal@gmail.com>
2023-09-28 15:14:36 +0300

    JS code style fixes

--- app/code/Alex/RequestSample/view/frontend/web/js/product/catalog-add-to-cart-mixin.js
+++ app/code/Alex/RequestSample/view/frontend/web/js/product/catalog-add-to-cart-mixin.js
@@ -4,7 +4,8 @@ define([
 ], function ($) {
     'use strict';
 
-    // see jQuery.mage.catalogAddToCart.prototype in your browser to get more information of how extending via mixins work
+    // see jQuery.mage.catalogAddToCart.prototype in your browser
+    // to get more information of how extending via mixins work
     return function (widget) {
         $.widget('mage.catalogAddToCart', widget, {
             /**
--- app/code/Alex/RequestSample/view/frontend/web/js/request-sample.js
+++ app/code/Alex/RequestSample/view/frontend/web/js/request-sample.js
@@ -19,13 +19,24 @@ define([
             $('body').on('alex_request_sample_clear_cookie', this.clearCookie.bind(this));
         },
 
+        /**
+         * Validate request and submit the form, if possible
+         */
         submitForm: function () {
             if (!this.validateForm()) {
                 validationAlert();
+
                 return;
             }
+            this.ajaxSubmit();
+        },
 
+        /**
+         * Submit request via AJAX. Add form key to the post data.
+         */
+        ajaxSubmit: function () {
             var formData = new FormData($(this.element).get(0));
+
             formData.append('form_key', $.mage.cookies.get('form_key'));
 
             $.ajax({
@@ -37,10 +48,12 @@ define([
                 dataType: 'json',
                 context: this,
 
+                /** @inheritDoc */
                 beforeSend: function () {
                     $('body').trigger('processStart');
                 },
 
+                /** @inheritDoc */
                 success: function (response) {
                     $('body').trigger('processStop');
                     alert({
@@ -49,26 +62,35 @@ define([
                     });
 
                     if (response.status === 'Success') {
-                        // can use this cookie to prevent from sending requests too often
+                        // Prevent from sending requests too often
                         $.mage.cookies.set(this.options.cookieName, true);
                     }
                 },
 
-                error: function (error) {
+                /** @inheritDoc */
+                error: function () {
                     $('body').trigger('processStop');
                     alert({
                         title: $.mage.__('Error'),
+                        /*eslint max-len: ["error", { "ignoreStrings": true }]*/
                         content: $.mage.__('Your request can not be submitted right now. Please, contact us directly via email or phone to get your Sample.')
                     });
                 }
-            })
+            });
 
         },
 
+        /**
+         * Validate request form
+         */
         validateForm: function () {
             return $(this.element).validation().valid();
         },
 
+        /**
+         *  Clear 'alex_request_sample_clear_cookie` cookie
+         *  when the event `alex_request_sample_clear_cookie` is triggered
+         */
         clearCookie: function () {
             $.mage.cookies.clear(this.options.cookieName);
         }
