395c4f42e1018f12fea1f4b9c9810d83575cd46c
Alex Salnikov <alexSal@gmail.com>
2023-10-18 12:02:56 +0300

    block content logic, changed parameters in widget layout, misc edits in phtml template

--- app/code/Alex/CustomWidget/Block/Widget/CustomWidget.php
+++ app/code/Alex/CustomWidget/Block/Widget/CustomWidget.php
@@ -2,8 +2,10 @@
 
 namespace Alex\CustomWidget\Block\Widget;
 
-use Magento\Framework\Phrase;
+use Magento\Cms\Api\BlockRepositoryInterface;
+use Magento\Cms\Model\Template\FilterProvider;
 use Magento\Framework\View\Element\Template;
+use Magento\Store\Model\StoreManagerInterface;
 use Magento\Widget\Block\BlockInterface;
 
 class CustomWidget extends Template implements BlockInterface
@@ -14,24 +16,59 @@ class CustomWidget extends Template implements BlockInterface
     protected $_template = "Alex_CustomWidget::widget/custom-widget.phtml";
 
     /**
-     * @param $code
-     * @return false|Phrase
+     * @var BlockRepositoryInterface
      */
-    public function getButtonTitle($code)
-    {
-        $result = false;
-        switch ($code) {
-            case 'title1':
-                $result = __("Title 1");
-                break;
-            case 'title2':
-                $result = __("Title 2");
-                break;
-            case 'title3':
-                $result = __("Title 3");
-                break;
-        }
+    private $blockRepository;
 
-        return $result;
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @var FilterProvider
+     */
+    private $filterProvider;
+
+    /**
+     * @param Template\Context $context
+     * @param BlockRepositoryInterface $blockRepository
+     * @param FilterProvider $filterProvider
+     * @param StoreManagerInterface $storeManager
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context         $context,
+        BlockRepositoryInterface $blockRepository,
+        FilterProvider           $filterProvider,
+        StoreManagerInterface    $storeManager,
+        array                    $data = []
+    )   {
+        parent::__construct($context, $data);
+        $this->filterProvider = $filterProvider;
+        $this->storeManager = $storeManager;
+        $this->blockRepository = $blockRepository;
+
+    }
+
+    /**
+     * @return string
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function getBlockContent()
+    {
+        $content = '';
+        if ($this->getStaticBlock()) {
+            $storeId = $this->storeManager->getStore()->getId();
+            $block = $this->blockRepository->getById($this->getStaticBlock());
+            $content = $this->filterProvider->getBlockFilter()->setStoreId($storeId)->filter($block->getContent());
+        }
+        return $content; // Return the content without echoing
+    }
+
+    public function getButtonTitle()
+    {
+        return $this->getData('button_title');
     }
 }
