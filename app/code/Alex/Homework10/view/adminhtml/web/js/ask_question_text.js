ec564974411438b53e39c9cb50051d1446e748c7
Alex Salnikov <alexSal@gmail.com>
2023-10-25 13:13:12 +0300

    added text string argument in xml layout in order to render it in html template with data-bind

--- app/code/Alex/Homework10/view/adminhtml/web/js/ask_question_text.js
+++ app/code/Alex/Homework10/view/adminhtml/web/js/ask_question_text.js
@@ -6,10 +6,6 @@ define([
     'use strict';
 
     return Component.extend({
-        defaults: {
-            template: 'Alex_Homework10/text_template',
-        },
-
         initialize: function () {
             this._super();
             console.log("I have been loaded!")
